cf65a39e746ade202df0035e360af149e7e9b3de
ILYA LAKEENKO <lakeen999@gmail.com>
2025-06-26 14:05:39 +0600

    created tests from CartTests and ProductCardTests

--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -1,7 +1,32 @@
 package test.java.tests;
 
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import pages.CartPage;
+import pages.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+@ExtendWith(AllureJunit5.class)
 public class CartTests {
-    /*
-    ОТКРЫТЬ КАРТОЧКУ ТОВАРА, ДОБАВИТЬ В КОРЗИНУ
-    */
+
+    MainPage mainPage = new MainPage();
+    CartPage cartPage = new CartPage();
+
+    @Test
+    public void addToCartTest() {
+        open("https://kinash.ru/");
+        mainPage.clickGeneralMenuElements("Баскетбол");
+        cartPage.clickFirstProduct()
+                .addToCart()
+                .checkCartCounterEquals(1);
+    }
+
+    @Test
+    public void clearCartTest() {
+        open("https://kinash.ru/cart");
+        cartPage.clearCart()
+                .checkCartCounterEquals(0);
+    }
 }
--- src/test/java/tests/ProductCardTests.java
+++ src/test/java/tests/ProductCardTests.java
@@ -1,9 +1,34 @@
 package test.java.tests;
 
-public class ProductCardTests {
-    /*
-    ОТКРЫТИЕ КАРТОЧКИ ОТОБРАЖЕНИЕ "оТЗЫВЫэ
-    ПЕРЕХОД ЧЕРЕЗ МЕНЮ НАПРИМЕР ОДЕЖДА, ВЫБРАТЬ ЭЛЕМЕНТ
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import pages.MainPage;
+import pages.ProductCardPage;
 
-     */
+import static com.codeborne.selenide.Selenide.open;
+
+@ExtendWith(AllureJunit5.class)
+public class ProductCardTests {
+
+    MainPage mainPage = new MainPage();
+    ProductCardPage productCard = new ProductCardPage();
+
+    @Test
+    public void checkReviewsSection() {
+        open("https://kinash.ru/product/volejbolnyj-myach-mikasa-v200w/");
+        productCard.scrollToReviews()
+                .clickReviewsTab()
+                .checkReviewButtonVisible();
+    }
+
+    @Test
+    public void checkProductCharacteristics() {
+        open("https://kinash.ru/");
+        mainPage.clickGeneralMenuElements("Обувь");
+        productCard.clickFirstProduct()
+                .scrollToCharacteristics()
+                .clickCharacteristicsTab()
+                .checkGenderCharacteristicPresent();
+    }
 }
