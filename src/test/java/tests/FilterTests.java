b0e3898c800cadf8d37250162e632b2cb460173e
ILYA LAKEENKO <lakeen999@gmail.com>
2025-06-26 14:05:05 +0600

    created tests from SearchTests and FilterTests

--- src/test/java/tests/FilterTests.java
+++ src/test/java/tests/FilterTests.java
@@ -1,7 +1,26 @@
 package test.java.tests;
 
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junitpioneer.jupiter.RetryingTest;
+import pages.FilterPage;
+import pages.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+@ExtendWith(AllureJunit5.class)
 public class FilterTests {
-    /*
-    ТЕСТ С ЛИСТОМ
-     */
+
+    MainPage mainPage = new MainPage();
+    FilterPage filterPage = new FilterPage();
+
+    @RetryingTest(maxAttempts = 3)
+    public void filterTest() {
+        open("https://kinash.ru/");
+        mainPage.clickGeneralMenuElements("Одежда");
+        String brand = filterPage.getFirstBrandName();
+        filterPage.selectFirstBrand()
+                .applyFilter()
+                .checkProductsContainBrand(brand);
+    }
 }
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,9 +1,23 @@
-package test.java.tests;
+package tests;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import pages.SearchPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class SearchTests {
-    /*
-    ПОИСК МЯЧ ВОЛЕБОЛЬНЫЙ - ПЕРЕНЕСТИ
-    ВВЕСТИ 5 ????? ОТОБРАЖАЕТСЯ "НИЧЕГО НЕ НАЙДЕНО
-    ТЕСТ ОДИН, НО С ПАРАМЕТРИЗАЦИЕЙ
-     */
+
+    @ParameterizedTest
+    @CsvSource({
+            "Мяч волейбольный,Мяч волейбольный",
+            "?????,Ничего не найдено"
+    })
+    public void parameterizedSearchTest(String request, String expectedText) {
+        SearchPage page = open("https://kinash.ru/", SearchPage.class);
+        page.setRequest(request)
+                .search();
+        $("body").shouldHave(text(expectedText));
+    }
 }
