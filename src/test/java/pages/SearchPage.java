3e6b6a81714f7fb565520041b5ec80a1e294631e
ILYA LAKEENKO <lakeen999@gmail.com>
2025-06-29 12:22:48 +0600

    updated SearchPage and SearchTests

--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,34 +1,46 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.matchText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SearchPage extends BasePage {
 
-    private SelenideElement firstResult = $("div.kea-products-container > div:first-child a.products-view-name-link");
+    private final SelenideElement firstResult = $("div.kea-products-container > div:first-child a.products-view-name-link");
+    private final SelenideElement noResultsText = $("div.search-empty h1");
 
+    @Step("Выполнение поиска по запросу: {request}")
     public SearchPage search() {
         searchField.setValue(request);
         searchBtn.click();
         return this;
     }
 
-    public SearchPage selectFirstResult(){
+    @Step("Выбор первого результата из результатов поиска")
+    public SearchPage selectFirstResult() {
         firstResult.click();
         return this;
     }
+
+    @Step("Проверка, что заголовок страницы содержит запрос: {request}")
     @Override
     public SearchPage checkTitle() {
         title.shouldHave(matchText(".*" + request + ".*"));
         return this;
     }
+
+    @Step("Установка поискового запроса: {request}")
     @Override
     public SearchPage setRequest(String request) {
         this.request = request;
         return this;
     }
 
-
+    @Step("Проверка, что отображается текст 'Ничего не найдено'")
+    public SearchPage checkNoResultsMessage() {
+        noResultsText.shouldHave(matchText("Ничего не найдено"));
+        return this;
+    }
 }
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,21 +1,26 @@
 package tests;
 
+import extensions.Retry;
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import pages.SearchPage;
 
-import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 
-public class SearchTests {
+@ExtendWith(AllureJunit5.class)
+public class SearchTests extends BaseTests {
 
     @ParameterizedTest
     @CsvSource({
             "Мяч волейбольный,Мяч волейбольный",
             "?????,Ничего не найдено"
     })
+    @Retry(3)
     public void parameterizedSearchTest(String request, String expectedText) {
-        SearchPage page = open("https://kinash.ru/", SearchPage.class);
+        SearchPage page = new SearchPage();
         page.setRequest(request)
                 .search();
         $("body").shouldHave(text(expectedText));
