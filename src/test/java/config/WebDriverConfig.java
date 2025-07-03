7314eb603d6badd400dfa108058fb2255c9a6c92
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-03 22:27:41 +0600

    changed default browser version

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -16,7 +16,7 @@ public interface WebDriverConfig extends Config {
         String browserSize();
 
         @Key("browser_version")
-        @DefaultValue("127.0")
+        @DefaultValue("100.0")
         String browserVersion();
 
         @Key("baseUrl")
