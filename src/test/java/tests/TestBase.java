c2fafe2800834d5512a35868a0c63727d75008f7
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-03 16:46:58 +0600

    updated test base

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,16 +2,26 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import config.Config;
+import config.WebDriverConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
+    private static WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+    public static String nikeTShirtUrl = baseUrl + "/products/nike-miler-short-sleave-running-top-futbolka-begovaya-temno-sinii";
+
     @BeforeAll
     static void setup() {
-        Configuration.browser = Config.get("browser");
-        Configuration.browserVersion = Config.get("browserVersion");
-        Configuration.browserSize = "1920x1080";
+        Configuration.browser = webDriverConfig.browser();
+        Configuration.browserVersion = webDriverConfig.browserVersion();
+        Configuration.browserSize = webDriverConfig.browserSize();
+        Configuration.baseUrl = webDriverConfig.baseUrl();
 
         String remoteUrl = Config.get("remoteUrl");
         if (remoteUrl != null && !remoteUrl.isEmpty()) {
@@ -22,5 +32,10 @@ public class TestBase {
             capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
         }
+    }
+
+    @BeforeEach
+    public void openSite() {
+        open(baseUrl);
     }
 }
