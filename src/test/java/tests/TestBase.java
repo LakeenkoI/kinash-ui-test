50d6a39e776ca48b360fe47c12d3eb8ec2d3f712
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-05 12:03:32 +0600

    updated test base for remote running

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,43 +2,57 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.Config;
 import config.WebDriverConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
+@ExtendWith(TestResultListener.class)
 public class TestBase {
 
     private static WebDriverConfig webDriverConfig = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
-    public static String nikeTShirtUrl = baseUrl + "/products/nike-dri-fit-uv-miler-short-sleave-running-top-futbolka-begovaya-chernyi";
+    public static String nikeTShirtUrl;
 
     @BeforeAll
     static void setup() {
+        Configuration.timeout = 7000;
         Configuration.browser = webDriverConfig.browser();
         Configuration.browserVersion = webDriverConfig.browserVersion();
         Configuration.browserSize = webDriverConfig.browserSize();
         Configuration.baseUrl = webDriverConfig.baseUrl();
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        nikeTShirtUrl = baseUrl + "/products/nike-dri-fit-uv-miler-short-sleave-running-top-futbolka-begovaya-chernyi";
 
-        String remoteUrl = Config.get("remoteUrl");
-        if (remoteUrl != null && !remoteUrl.isEmpty()) {
-            Configuration.remote = remoteUrl;
-
+        boolean isRemote = webDriverConfig.isRemote();
+        if (isRemote) {
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
+            capabilities.setBrowserName(webDriverConfig.browser());
+            capabilities.setVersion(webDriverConfig.browserVersion());
+
+            Map<String, Object> selenoidOptions = new HashMap<>();
+            selenoidOptions.put("enableVNC", true);
+            selenoidOptions.put("enableVideo", true);
+            capabilities.setCapability("selenoid:options", selenoidOptions);
+
             Configuration.browserCapabilities = capabilities;
+            Configuration.remote = webDriverConfig.remoteUrl();
         }
     }
 
     @BeforeEach
     public void openSite() {
-        open(baseUrl);
+        step("Открываем основную страницу сайта", () -> {
+            open(baseUrl);
+        });
     }
 }
