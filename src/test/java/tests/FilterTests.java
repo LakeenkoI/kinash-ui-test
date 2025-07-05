4126fc7717cc2d70d3fd1f295eb63e8da8b0c7a4
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-05 11:59:41 +0600

    updated filter tests

--- src/test/java/tests/FilterTests.java
+++ src/test/java/tests/FilterTests.java
@@ -1,7 +1,9 @@
 package tests;
 
 import data.GeneralMenuElements;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -13,14 +15,16 @@ import pages.MainPage;
 import static pages.CoreElementsPage.clickGeneralMenuElements;
 
 @ExtendWith(AllureJunit5.class)
+@Owner("LakeenkoI")
+@Story("Фильтр")
+@Feature("Kinash")
+@Tag("Filter")
 public class FilterTests extends TestBase {
 
     MainPage mainPage = new MainPage();
     FilterPage filterPage = new FilterPage();
     @Test
     @DisplayName("В названии отфильтрованных товаров содержится название бренда из фильтра")
-    @Owner("lakeenkoi")
-    @Tag("Filter")
     public void filterTest() {
         clickGeneralMenuElements(GeneralMenuElements.CLOTHING);
         String brand = filterPage.getFirstBrandName();
