ea03a60e26fd1841c8f7e1b346362cb48a27cb6c
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-08 10:19:38 +0600

    updated main page and tests

--- src/main/java/com/lakeenko/pages/MainPage.java
+++ src/main/java/com/lakeenko/pages/MainPage.java
@@ -4,14 +4,21 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class MainPage  {
 
-    SelenideElement mainPageTitle = $("div.col-xs-slim div[style]");
-    SelenideElement logo = $("#logo");
-    SelenideElement footer = $("footer");
+    private final SelenideElement mainPageTitle = $("div.col-xs-slim div[style]");
+    private final SelenideElement logo = $("#logo");
+    private final SelenideElement footer = $("footer");
 
+    @Step("Открываем основную страницу сайта")
+    public MainPage openSite() {
+        open(baseUrl);
+        return this;
+    }
     @Step("Проверяем, что заголовок, логотип и футер главной страницы отображаются")
     public void checkMainPageElements() {
         mainPageTitle.shouldBe(visible);
--- src/test/java/com/lakeenko/tests/smoke/MainPageTests.java
+++ src/test/java/com/lakeenko/tests/smoke/MainPageTests.java
@@ -2,6 +2,7 @@ package com.lakeenko.tests.smoke;
 
 import com.lakeenko.tests.TestBase;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Link;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit5.AllureJunit5;
@@ -12,15 +13,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 
 @ExtendWith(AllureJunit5.class)
+@Link(name = "https://kinash.ru/", url = "https://kinash.ru/")
 @Owner("LakeenkoI")
-@Story("Главная страница")
 @Feature("Kinash")
+@Story("Главная страница")
 @Tag("Main")
+@Tag("Smoke")
 public class MainPageTests extends TestBase {
 
     @Test
-    @DisplayName("На главное странице отображаются: корзина, футтер, лого, тайтл")
+    @DisplayName("На главной странице отображаются: корзина, футтер, лого, тайтл")
     public void mainPageElementsTest() {
-        mainPage.checkMainPageElements();
+        mainPage.openSite()
+                .checkMainPageElements();
     }
 }
