a24664fc2a382b902b79edd511d368c04722985a
ILYA LAKEENKO <lakeen999@gmail.com>
2025-07-08 10:16:19 +0600

    updated configures webdriver

---
+++ src/main/java/com/lakeenko/config/DriverConfigurator.java
@@ -0,0 +1,41 @@
+package com.lakeenko.config;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.lakeenko.listeners.CustomSelenideListener;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DriverConfigurator {
+
+    private static final WebDriverConfig CONFIG = ConfigProvider.CONFIG;
+
+    public static void configure() {
+        Configuration.timeout = 7000;
+        Configuration.browser = CONFIG.browser();
+        Configuration.browserVersion = CONFIG.browserVersion();
+        Configuration.browserSize = CONFIG.browserSize();
+        Configuration.baseUrl = CONFIG.baseUrl();
+
+        if (CONFIG.isRemote()) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            Map<String, Object> selenoidOptions = new HashMap<>();
+            selenoidOptions.put("enableVNC", true);
+            selenoidOptions.put("enableVideo", true);
+            capabilities.setCapability("selenoid:options", selenoidOptions);
+
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = CONFIG.remoteUrl();
+        }
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        SelenideLogger.addListener("Custom", new CustomSelenideListener());
+    }
+}
